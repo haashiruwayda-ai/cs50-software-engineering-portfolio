f61d60775b47bf1611cfd29e587e71f3f518fe76
Ruwayda <haashiruwayda@gmail.com>
2026-08-31 14:43:44 +0100

    Implement Mario pyramid program in C

---
+++ mario-less/mario.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+{int n;
+do
+{
+    n =  get_int("Positive number:");
+}
+while (n < 1 || n > 8 );
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j <  n - 1 - i; j++)
+    {
+        printf(" ");
+    }
+    for (int j = 0; j < i + 1; j++)
+    {
+    printf("#");
+    }
+    printf("\n");
+}
+}
